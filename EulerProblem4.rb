13e62da908e6eb6412556edae7add65b4b8e7533
williamkennedy <williamkennedyjnr@gmail.com>
2015-05-25 13:20:42 +0000

    Project 4 completed

--- EulerProblem4.rb
+++ EulerProblem4.rb
@@ -2,3 +2,29 @@
 
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
+def palindrome(bigNum,smallNum)
+  palindromes = []
+  multiples = []#collecting the number of multiples
+  
+  bigNum.downto(smallNum) do |a| 
+    bigNum.downto(smallNum) do |b|    
+      multipliedNum = a * b
+      multiples.push(multipliedNum.to_s)
+    end
+  end
+    
+  multiples.each do |n| #collecting the number of pallidromes   
+    k = n.reverse
+    if k.to_i ==  n.to_i#compare the number
+     palindromes.push(k.to_i)
+    end
+  end
+  puts palindromes.max
+ 
+  
+end
+
+palindrome(999,100)
+
+
+
